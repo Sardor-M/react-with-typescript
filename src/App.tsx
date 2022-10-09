18b4c01e9609bad8813d89fcc64244e962e4e0d3
Sardor Madaminov <sardor0968@gmail.com>
2022-10-09 23:14:48 +0900

    using state: added

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,45 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  // const [number, setNumber] = useState<number | string | boolean>(2);
+
+  // const changeNum = () => {
+  //   setNumber('33');
+  // }  
+  // {
+  //   name: 'Steve Jobs',
+  //   url:'',
+  //   age: 44,
+  //   note: 'So far the best team player in the University'
+  // }, 
+  // {
+  //   name: 'John Nideas',
+  //   url:'',
+  //   age: 51,
+  //   note: 'He is the best team leader in the basketball club'
+  // }, 
+
+  interface ofState {
+    people: {
+      name: string,
+      age: number,
+      id?: number
+      url: string
+      note?: string
+    }[]
+  }
+
+  const [people, setPeople] = useState<ofState['people']>([])
+
+  people.map(person => {
+    person.age
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> People which are invited to school event.</h1>
     </div>
   );
 }
